d6a07dcf945f532e3504621c1e228ad8ae59b638
Robin-Kurian <dev.robinkurian@gmail.com>
2025-01-08 00:59:11 +0530

    Remove Hero component and update page layout
    - Deleted the Hero component from the project.
    - Updated the main page to replace the Hero section with a new heading "Shop" for improved clarity and focus.
    
    These changes streamline the user interface and enhance the overall user experience.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
-import Hero from "@/components/hero";
-
 export default async function Home() {
   return (
     <>
-      <Hero />
       <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
+        <h2 className="font-medium text-xl h-full my-20">Shop</h2>
       </main>
     </>
   );
