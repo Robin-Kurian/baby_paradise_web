81914e409f17a51faafd6410658e2b1f658302e0
Robin-Kurian <dev.robinkurian@gmail.com>
2025-01-08 11:26:39 +0530

    Update layout and page heading for improved UI consistency
    - Adjusted layout styles to include justification in the main content area.
    - Changed the page heading from "Shop" to "Notes" for better context.
    - Enhanced footer background color for improved visibility.
    
    These changes contribute to a more cohesive user interface and clarify the purpose of the main page.

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <main className="min-h-screen flex flex-col items-center">
-            <div className="flex-1 w-full flex flex-col gap-2 items-center">
+            <div className="flex-1 w-full flex flex-col gap-2 items-center justify-between">
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
@@ -49,7 +49,7 @@ export default function RootLayout({
                 {children}
               </div>
 
-              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-2 py-8">
+              <footer className="w-full bg-slate-200 flex items-center justify-center border-t mx-auto text-center text-xs gap-2 py-8">
                 <p>
                   Developed by{" "}
                   <a
