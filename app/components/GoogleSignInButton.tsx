35b5dad02322081b4cddcd21056483e7b4ae4127
Robin-Kurian <dev.robinkurian@gmail.com>
2025-01-09 15:18:35 +0530

    Add Google Sign-In and Sign-Out components for user authentication

---
+++ app/components/GoogleSignInButton.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useState } from "react";
+import { createClient } from "@/utils/supabase/client";
+import { Button } from "@/components/ui/button";
+import { toast } from "@/hooks/use-toast";
+import { Icons } from "@/components/icons";
+
+interface GoogleSignInButtonProps {
+  next?: string | null;
+}
+
+const GoogleSignInButton: React.FC<GoogleSignInButtonProps> = ({ next }) => {
+  const [isGoogleLoading, setIsGoogleLoading] = useState<boolean>(false);
+  const supabase = createClient();
+
+  async function signInWithGoogle() {
+    setIsGoogleLoading(true);
+    try {
+      const { error } = await supabase.auth.signInWithOAuth({
+        provider: "google",
+        options: {
+          redirectTo: `${window.location.origin}/auth/callback${
+            next ? `?next=${encodeURIComponent(next)}` : ""
+          }`,
+        },
+      });
+
+      if (error) {
+        throw error;
+      }
+    } catch (error) {
+      toast({
+        title: "Please try again.",
+        description: "There was an error logging in with Google.",
+        variant: "destructive",
+      });
+      setIsGoogleLoading(false);
+    }
+  }
+
+  return (
+    <Button
+      type="button"
+      variant="outline"
+      onClick={signInWithGoogle}
+      disabled={isGoogleLoading}
+    >
+      {isGoogleLoading ? (
+        <Icons.loaderCircle className="mr-2 size-4 animate-spin" />
+      ) : (
+        <Icons.google className="mr-2 size-6" />
+      )}{" "}
+      Sign in with Google
+    </Button>
+  );
+};
+
+export default GoogleSignInButton;
